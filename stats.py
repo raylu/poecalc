0abd79c946f66b908039784a60704b0c17aecec0
raylu <90059+raylu@users.noreply.github.com>
2022-10-28 10:23:57 -0700

    spoof user-agent more thoroughly
    the website started serving 403s for just 'Mozilla/5.0'
    
    interestingly, it accepts 'curl/7.85.0'

--- stats.py
+++ stats.py
@@ -39,7 +39,7 @@ class Stats:
 
 
 client = httpx.Client(timeout=15)
-client.headers['User-Agent'] = 'Mozilla/5.0'
+client.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'
 
 
 def fetch_stats(account, character_name) -> tuple[Stats, dict, dict]:
