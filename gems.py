223fc0cb3287af392cd5d47aebdb187d46a83da4
Liberator <liberatorist@gmail.com>
2023-02-04 11:35:14 +0100

    witchfire brew bugfix

--- gems.py
+++ gems.py
@@ -397,6 +397,10 @@ def item_gem_dict(mod_string: str) -> dict:
 		name = m.group(2) + ' Support'
 		support = True
 		level = m.group(1)
+	elif m := re.match(r'Grants Level (\d+) Curse Aura', mod_string):
+		name = m.group(2)
+		support = False
+		level = m.group(1)
 	elif m := re.match(r'Grants Level (\d+) (.+) Skill', mod_string):
 		name = m.group(2)
 		support = False
